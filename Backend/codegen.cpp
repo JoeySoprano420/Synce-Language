5b79618c66a49f0fb42226e3d03c27b4278984c6
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-13 02:39:01 -0400

    Update codegen.cpp

--- Backend/codegen.cpp
+++ Backend/codegen.cpp
@@ -44,3 +44,161 @@ std::string generate_nasm(const std::string& ir) {
 
     return out.str();
 }
+
+std::string generate_cpp(const std::string& ir) {
+    std::ostringstream out;
+    out << "#include <iostream>\n";
+    out << "int main() {\n";
+    auto lines = split_lines(ir);
+    for (const auto& line : lines) {
+        std::smatch match;
+        if (std::regex_match(line, match, std::regex("0[0-9]+ DECL (\\w+)"))) {
+            std::string var = match[1];
+            out << "    int " << var << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ LIT (\\d+)"))) {
+            std::string val = match[1];
+            out << "    int value = " << val << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ ID (\\w+)"))) {
+            std::string id = match[1];
+            out << "    std::cout << \"" << id << "\" << std::endl;\n";
+        }
+    }
+    out << "    return 0;\n";
+    out << "}\n";
+    return out.str();
+}
+
+std::string generate_python(const std::string& ir) {
+    std::ostringstream out;
+    out << "def main():\n";
+    auto lines = split_lines(ir);
+    for (const auto& line : lines) {
+        std::smatch match;
+        if (std::regex_match(line, match, std::regex("0[0-9]+ DECL (\\w+)"))) {
+            std::string var = match[1];
+            out << "    " << var << " = None\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ LIT (\\d+)"))) {
+            std::string val = match[1];
+            out << "    value = " << val << "\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ ID (\\w+)"))) {
+            std::string id = match[1];
+            out << "    print('" << id << "')\n";
+        }
+    }
+    out << "if __name__ == '__main__':\n";
+    out << "    main()\n";
+    return out.str();
+}
+
+std::string generate_go(const std::string& ir) {
+    std::ostringstream out;
+    out << "package main\n";
+    out << "import \"fmt\"\n";
+    out << "func main() {\n";
+    auto lines = split_lines(ir);
+    for (const auto& line : lines) {
+        std::smatch match;
+        if (std::regex_match(line, match, std::regex("0[0-9]+ DECL (\\w+)"))) {
+            std::string var = match[1];
+            out << "    var " << var << " int\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ LIT (\\d+)"))) {
+            std::string val = match[1];
+            out << "    value := " << val << "\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ ID (\\w+)"))) {
+            std::string id = match[1];
+            out << "    fmt.Println(\"" << id << "\")\n";
+        }
+    }
+    out << "}\n";
+    return out.str();
+}
+
+std::string generate_rust(const std::string& ir) {
+    std::ostringstream out;
+    out << "fn main() {\n";
+    auto lines = split_lines(ir);
+    for (const auto& line : lines) {
+        std::smatch match;
+        if (std::regex_match(line, match, std::regex("0[0-9]+ DECL (\\w+)"))) {
+            std::string var = match[1];
+            out << "    let mut " << var << ": i32;\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ LIT (\\d+)"))) {
+            std::string val = match[1];
+            out << "    let value = " << val << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ ID (\\w+)"))) {
+            std::string id = match[1];
+            out << "    println!(\"{}\", \"" << id << "\");\n";
+        }
+    }
+    out << "}\n";
+    return out.str();
+}
+
+std::string generate_javascript(const std::string& ir) {
+    std::ostringstream out;
+    out << "function main() {\n";
+    auto lines = split_lines(ir);
+    for (const auto& line : lines) {
+        std::smatch match;
+        if (std::regex_match(line, match, std::regex("0[0-9]+ DECL (\\w+)"))) {
+            std::string var = match[1];
+            out << "    let " << var << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ LIT (\\d+)"))) {
+            std::string val = match[1];
+            out << "    let value = " << val << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ ID (\\w+)"))) {
+            std::string id = match[1];
+            out << "    console.log('" << id << "');\n";
+        }
+    }
+    out << "}\n";
+    out << "main();\n";
+    return out.str();
+}
+
+std::string generate_java(const std::string& ir) {
+    std::ostringstream out;
+    out << "public class Main {\n";
+    out << "    public static void main(String[] args) {\n";
+    auto lines = split_lines(ir);
+    for (const auto& line : lines) {
+        std::smatch match;
+        if (std::regex_match(line, match, std::regex("0[0-9]+ DECL (\\w+)"))) {
+            std::string var = match[1];
+            out << "        int " << var << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ LIT (\\d+)"))) {
+            std::string val = match[1];
+            out << "        int value = " << val << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ ID (\\w+)"))) {
+            std::string id = match[1];
+            out << "        System.out.println(\"" << id << "\");\n";
+        }
+    }
+    out << "    }\n";
+    out << "}\n";
+    return out.str();
+}
+
+std::string generate_c(const std::string& ir) {
+    std::ostringstream out;
+    out << "#include <stdio.h>\n";
+    out << "int main() {\n";
+    auto lines = split_lines(ir);
+    for (const auto& line : lines) {
+        std::smatch match;
+        if (std::regex_match(line, match, std::regex("0[0-9]+ DECL (\\w+)"))) {
+            std::string var = match[1];
+            out << "    int " << var << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ LIT (\\d+)"))) {
+            std::string val = match[1];
+            out << "    int value = " << val << ";\n";
+        } else if (std::regex_match(line, match, std::regex("0[0-9]+ ID (\\w+)"))) {
+            std::string id = match[1];
+            out << "    printf(\"%s\\n\", \"" << id << "\");\n";
+        }
+    }
+    out << "    return 0;\n";
+    out << "}\n";
+    return out.str();
+}
+
