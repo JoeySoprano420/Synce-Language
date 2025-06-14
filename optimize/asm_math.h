f300023400b9f5a37f827b46207d3079610dbcb1
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-13 20:55:54 -0400

    Create asm_math.h

---
+++ optimize/asm_math.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstdint>
+
+class AsmMath {
+public:
+    static int fastMul(int a, int b);
+    static int fastAdd(int a, int b);
+    static float inverseRoot(float value);
+};
